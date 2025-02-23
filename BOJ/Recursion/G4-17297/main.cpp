45909d16a647e63ea0feff55a12ab58ef32bb42f
footprint\Donghyeon <gusdldi300@naver.com>
2025-02-23 13:03:03 +0900

    no message

--- BOJ/Recursion/G4-17297/main.cpp
+++ BOJ/Recursion/G4-17297/main.cpp
@@ -1,21 +1,6 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <unordered_map>
-
-/*
-
-messi(1): Messi
-messi(2)​​: Messi Gimossi
-messi(3)​​​​​​: Messi Gimossi Messi
-messi(4): Messi Gimossi Messi Messi Gimossi
-messi(5): Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi
-messi(6): Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi
-messi(7): Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi
-messi(8): Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi
-messi(9): Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi
-          Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi Messi Gimossi Messi Gimossi Messi
-*/
 
 int main()
 {
@@ -25,14 +10,14 @@ int main()
 
     std::string previousShout = "Messi";
     std::string currentShout = "Messi Gimossi";
-    
+
     char foundCharacter;
     if (shoutIndex < previousShout.size())
     {
         foundCharacter = previousShout[shoutIndex];
         goto character_found;
     }
-    
+
     if (shoutIndex < currentShout.size())
     {
         foundCharacter = currentShout[shoutIndex];
@@ -43,10 +28,10 @@ int main()
     {
         std::string nextShout;
         nextShout.reserve(previousShout.size() + currentShout.size() + 1);
-        nextShout += currentShout;
-        nextShout += ' ';
         nextShout += previousShout;
-        
+        nextShout += ' ';
+        nextShout += currentShout;
+
         if (shoutIndex < nextShout.size())
         {
             foundCharacter = nextShout[shoutIndex];
@@ -58,7 +43,7 @@ int main()
     }
 
 character_found:
-    
+
     if (foundCharacter == ' ')
     {
         std::cout << "Messi Messi Gimossi";
