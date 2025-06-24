8b85fe963b9a086936cec66ef9db26f6fa80f070
footprint\Donghyeon <gusdldi300@naver.com>
2025-06-24 15:33:28 +0900

    solved

--- BOJ/Recursion/G3-26110/main.cpp
+++ BOJ/Recursion/G3-26110/main.cpp
@@ -1,83 +1,42 @@
 
 #include <iostream>
 #include <string>
-#include <unordered_set>
 
-static unsigned int sRemoveCount = 0;
+#define MAX_REMOVE_COUNT (3)
 
-bool IsPalindromeRecursive(int removeStartIndex, int currentRemoveCount, std::unordered_set<int>& removedIndexes, const std::string& testString, char* fixedString)
+int GetPalindromeRemoveCountRecursive(int leftStartIndex, int rightStartIndex, int removeCount, const std::string& testString)
 {
-    if (currentRemoveCount == 0)
+    if (removeCount > MAX_REMOVE_COUNT)
     {
-        // Check palindrome
-        int stringSize = testString.size();
-        char* newString = fixedString;
-        for (int i = 0; i < stringSize; ++i)
-        {
-            if (removedIndexes.find(i) != removedIndexes.end())
-            {   
-                continue;
-            }
-
-            *newString++ = testString[i];
-        }
-
-        int leftIndex = 0;
-        int rightIndex = testString.size() - sRemoveCount - 1;
-        while (leftIndex < rightIndex)
-        {
-            if (fixedString[leftIndex] != fixedString[rightIndex])
-            {
-                return false;
-            }
-
-            ++leftIndex;
-            --rightIndex;
-        }
-
-        /*
-        while (true)
-        {
-            while (removedIndexes.find(leftIndex) != removedIndexes.end())
-            {
-                ++leftIndex;
-            }
-
-            while (removedIndexes.find(rightIndex) != removedIndexes.end())
-            {
-                --rightIndex;            
-            }
-
-            if (leftIndex >= rightIndex)
-            {
-                break;
-            }
-
-            if (testString[leftIndex] != testString[rightIndex])
-            {
-                return false;
-            }
-
-            ++leftIndex;
-            --rightIndex;
-        }
-        */
-
-        return true;
+        return MAX_REMOVE_COUNT + 1;
     }
 
-    int stringSize = (int)testString.size();
-    for (int i = removeStartIndex; i < stringSize; ++i)
+    int leftIndex = leftStartIndex;
+    int rightIndex = rightStartIndex;
+
+    bool bPalindrome = true;
+    while (leftIndex < rightIndex)
     {
-        removedIndexes.insert(i);
-        if (IsPalindromeRecursive(i + 1, currentRemoveCount - 1, removedIndexes, testString, fixedString))
+        if (testString[leftIndex] != testString[rightIndex])
         {
-            return true;
+            bPalindrome = false;
+
+            break;
         }
-        removedIndexes.erase(i);
+
+        ++leftIndex;
+        --rightIndex;
     }
 
-    return false;
+    if (bPalindrome)
+    {
+        return removeCount;
+    }
+
+    int leftRemovedCount = GetPalindromeRemoveCountRecursive(leftIndex + 1, rightIndex, removeCount + 1, testString);
+    int rightRemovedCount = GetPalindromeRemoveCountRecursive(leftIndex, rightIndex - 1, removeCount + 1, testString);
+
+    return leftRemovedCount <= rightRemovedCount ? leftRemovedCount : rightRemovedCount;
 }
 
 int main()
@@ -85,24 +44,15 @@ int main()
     std::string testString;
     std::cin >> testString;
 
-    const unsigned int MAX_REMOVE_COUNT = 3;
-    const unsigned int MAX_STRING_SIZE = 100001;
-    
-    char fixedString[MAX_STRING_SIZE];
-    std::unordered_set<int> removedIndexes;
-
-    bool bPalindrome = false;
-    for (sRemoveCount = 0; sRemoveCount <= MAX_REMOVE_COUNT; sRemoveCount++)
+    int minRemovedCount = GetPalindromeRemoveCountRecursive(0, testString.size() - 1, 0, testString);
+    if (minRemovedCount <= MAX_REMOVE_COUNT)
     {
-        if (IsPalindromeRecursive(0, sRemoveCount, removedIndexes, testString, fixedString))
-        {
-            std::cout << sRemoveCount;
-
-            return 0;
-        }
+        std::cout << minRemovedCount;
     }
-
-    std::cout << -1;
+    else
+    {
+        std::cout << -1;
+    }
     
     return 0;
 }
