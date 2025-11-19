d1cdf355bc79c2ec0349fbc1b1e7fff4df87383f
footprint\Donghyeon <gusdldi300@naver.com>
2025-11-20 00:14:56 +0900

    solved

---
+++ BOJ/DP/S4-2670/main.cpp
@@ -0,0 +1,34 @@
+
+#include <cstdio>
+#include <iostream>
+
+#define MAX_FLOAT_COUNT (10000U)
+#define FLOAT_ROUND_ADD_OFFSET (0.0005)
+
+int main()
+{
+    double floats[MAX_FLOAT_COUNT] = { 0.f, };
+
+    unsigned int floatCount = 0;
+    std::cin >> floatCount;
+
+    for (unsigned int i = 0; i < floatCount; ++i)
+    {
+        std::cin >> floats[i];
+    }
+
+    double maxMultipleFloat = floats[0];
+    double lastFloat = floats[0];
+
+    for (unsigned int i = 1; i < floatCount; ++i)
+    {
+        double compareFloat = lastFloat * floats[i];
+        lastFloat = (compareFloat >= floats[i] ? compareFloat : floats[i]);
+
+        maxMultipleFloat = (lastFloat > maxMultipleFloat ? lastFloat : maxMultipleFloat);
+    }
+
+    printf("%.3f", (maxMultipleFloat));
+
+    return 0;
+}
