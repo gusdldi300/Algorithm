f186e0126f3eb60279827b0dfcaf28d7c9aa8b40
footprint\Donghyeon <gusdldi300@naver.com>
2025-12-25 22:38:38 +0900

    reviewed

--- BOJ/DP/G5-1577/main.cpp
+++ BOJ/DP/G5-1577/main.cpp
@@ -1,120 +1,57 @@
 #include <iostream>
-#include <unordered_set>
-#include <string>
-#include <queue>
+#include <set>
 
 #define MAX_STRING_ROAD_SIZE (8U)
 #define MAX_MAP_SIZE (101)
 #define MAX_MOVE_POSITIONS_SIZE (2)
 
-struct Position
-{
-    int Row;
-    int Col;
-};
 
 static long long sCases[MAX_MAP_SIZE][MAX_MAP_SIZE] = { 0, };
-
-static std::unordered_set<std::string> sRepairRoads;
-
-static unsigned int sMapSizeRow;
-static unsigned int sMapSizeCol;
-
-// N, E
-static Position MOVE_POSITIONS[MAX_MOVE_POSITIONS_SIZE] = { { -1, 0 }, { 0, 1 } };
-
-static void TransformRoadToString(const Position& start, const Position& end, std::string* outRoadString)
-{
-    outRoadString->append(std::to_string(start.Row));
-    outRoadString->append(",");
-    outRoadString->append(std::to_string(start.Col));
-
-    outRoadString->append("-");
-
-    outRoadString->append(std::to_string(end.Row));
-    outRoadString->append(",");
-    outRoadString->append(std::to_string(end.Col));
-}
+static std::set<std::pair<std::pair<unsigned int, unsigned int>, std::pair<unsigned int, unsigned int>>> sRepairRoads;
 
 int main()
 {
-    std::cin >> sMapSizeCol >> sMapSizeRow;
-    sMapSizeRow++;
-    sMapSizeCol++;
+    unsigned int mapSizeX;
+    unsigned int mapSizeY;
+    std::cin >> mapSizeX >> mapSizeY;
+    mapSizeX++;
+    mapSizeY++;
 
-    unsigned int repairingRoadsCount;
-    std::cin >> repairingRoadsCount;
+    unsigned int repairRoadsCount;
+    std::cin >> repairRoadsCount;
 
-    for (unsigned int i = 0; i < repairingRoadsCount; ++i)
+    for (unsigned int i = 0; i < repairRoadsCount; ++i)
     {
-        // Transform coordinates: x, y -> M - y, x
-        Position startPosition;
+        unsigned int startX;
+        unsigned int startY;
+        std::cin >> startX >> startY;
+        
+        unsigned int endX;
+        unsigned int endY;
+        std::cin >> endX >> endY;
 
-        std::cin >> startPosition.Col >> startPosition.Row;
-        startPosition.Row = sMapSizeRow - 1 - startPosition.Row;
-
-        Position endPosition;
-
-        std::cin >> endPosition.Col >> endPosition.Row;
-        endPosition.Row = sMapSizeRow - 1 - endPosition.Row;
-
-        std::string repairRoadString;
-        repairRoadString.reserve(MAX_STRING_ROAD_SIZE);
-        TransformRoadToString(startPosition, endPosition, &repairRoadString);
-        sRepairRoads.insert(repairRoadString);
-
-        std::string reveredRoadString;
-        reveredRoadString.reserve(MAX_STRING_ROAD_SIZE);
-        TransformRoadToString(endPosition, startPosition, &reveredRoadString);
-        sRepairRoads.insert(reveredRoadString);
+        sRepairRoads.insert({ { startX, startY }, { endX, endY } });
+        sRepairRoads.insert({ { endX, endY }, { startX, startY } });
     }
 
-    bool visited[MAX_MAP_SIZE][MAX_MAP_SIZE] = { false, };
-
-    std::queue<Position> positionQueue;
-    Position startPosition = { sMapSizeRow - 1, 0 };
-    sCases[startPosition.Row][startPosition.Col] = 1;
-
-    positionQueue.push(startPosition);
-    
-    while (positionQueue.empty() == false)
+    sCases[0][0] = 1;
+    for (unsigned int y = 0; y < mapSizeY; ++y)
     {
-        Position position = positionQueue.front();
-        positionQueue.pop();
-
-        for (unsigned int i = 0; i < MAX_MOVE_POSITIONS_SIZE; ++i)
+        for (unsigned int x = 0; x < mapSizeX; ++x)
         {
-            Position nextPosition;
-            nextPosition.Row = position.Row + MOVE_POSITIONS[i].Row;
-            nextPosition.Col = position.Col + MOVE_POSITIONS[i].Col;
-
-            if (nextPosition.Row >= sMapSizeRow || nextPosition.Col >= sMapSizeCol)
+            if (x < (mapSizeX - 1) && sRepairRoads.find({ { x, y }, { x + 1, y } }) == sRepairRoads.end())
             {
-                continue;
+                sCases[y][x + 1] += sCases[y][x];
             }
 
-            std::string roadString;
-            roadString.reserve(MAX_STRING_ROAD_SIZE);
-            TransformRoadToString(position, nextPosition, &roadString);
-
-            if (sRepairRoads.find(roadString) != sRepairRoads.end())
+            if (y < (mapSizeY - 1) && sRepairRoads.find({ { x, y }, { x, y + 1 } }) == sRepairRoads.end())
             {
-                continue;
+                sCases[y + 1][x] += sCases[y][x];
             }
-
-            sCases[nextPosition.Row][nextPosition.Col] += sCases[position.Row][position.Col];
-
-            if (visited[nextPosition.Row][nextPosition.Col])
-            {
-                continue;
-            }
-
-            visited[nextPosition.Row][nextPosition.Col] = true;
-            positionQueue.push(nextPosition);
         }
     }
 
-    std::cout << sCases[0][sMapSizeCol - 1];
+    std::cout << sCases[mapSizeY - 1][mapSizeX - 1];
 
     return 0;
 }
