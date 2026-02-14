410f745c5d1f80dfe6ac9a730e1b31f7e203485e
footprint\Donghyeon <gusdldi300@naver.com>
2026-02-15 08:57:11 +0900

    reviewed

--- BOJ/DP/G4-2073/main.cpp
+++ BOJ/DP/G4-2073/main.cpp
@@ -1,75 +1,33 @@
-
 #include <iostream>
-#include <unordered_map>
+#include <climits>
 
 #define MAX_PIPES_COUNT (361U)
+#define MAX_LENGTH (100001U)
 
 int main()
 {
     unsigned int targetLength;
     unsigned int pipesCount;
-    
+
     std::cin >> targetLength >> pipesCount;
 
-    std::unordered_map<unsigned int, unsigned int> lastLengthMaxCapacityMap;
-    std::unordered_map<unsigned int, unsigned int> lengthMaxCapacityMap;
+    unsigned int maxCapacites[MAX_LENGTH] = { 0, };
+    maxCapacites[0] = UINT_MAX;
 
-    unsigned int pipeLength;
-    unsigned int pipeCapacity;
-
-    std::cin >> pipeLength >> pipeCapacity;
-    if (pipeLength <= targetLength)
+    for (unsigned int pipe = 0; pipe < pipesCount; ++pipe)
     {
-        lastLengthMaxCapacityMap.insert(std::pair<unsigned int, unsigned int>(pipeLength, pipeCapacity));
-    }
-    
-    for (unsigned int pipe = 1; pipe < pipesCount; ++pipe)
-    {
+        unsigned int pipeLength;
+        unsigned int pipeCapacity;
         std::cin >> pipeLength >> pipeCapacity;
-        
-        if (pipeLength > targetLength)
+
+        for (unsigned int length = targetLength; length >= pipeLength; --length)
         {
-            continue;
+            maxCapacites[length] = std::max(maxCapacites[length],
+                                            std::min(pipeCapacity, maxCapacites[length - pipeLength]));
         }
-
-        unsigned int newPipeCapacity = pipeCapacity;
-        if (lengthMaxCapacityMap.find(pipeLength) != lengthMaxCapacityMap.end())
-        {
-            newPipeCapacity = std::max(newPipeCapacity, lengthMaxCapacityMap[pipeLength]);
-        }
-
-        lengthMaxCapacityMap[pipeLength] = newPipeCapacity;
-
-        for (auto pipeInfo : lastLengthMaxCapacityMap)
-        {
-            unsigned int newPipeCapacity = pipeInfo.second;
-            if (lengthMaxCapacityMap.find(pipeInfo.first) != lengthMaxCapacityMap.end())
-            {
-                newPipeCapacity = std::max(newPipeCapacity, lengthMaxCapacityMap[pipeInfo.first]);
-            }
-
-            lengthMaxCapacityMap[pipeInfo.first] = newPipeCapacity;
-
-            unsigned int connectedLength = pipeInfo.first + pipeLength;
-            if (connectedLength > targetLength)
-            {
-                continue;
-            }
-
-            unsigned int connectedPipeCapacity = std::min(lastLengthMaxCapacityMap[pipeInfo.first], pipeCapacity);
-            if (lengthMaxCapacityMap.find(connectedLength) != lengthMaxCapacityMap.end())
-            {
-                connectedPipeCapacity = std::max(connectedPipeCapacity, lengthMaxCapacityMap[connectedLength]);
-            }
-
-            lengthMaxCapacityMap[connectedLength] = connectedPipeCapacity;
-        }
-
-        std::swap(lastLengthMaxCapacityMap, lengthMaxCapacityMap);
-        lengthMaxCapacityMap.clear();
     }
 
-    std::cout << lastLengthMaxCapacityMap[targetLength];
+    std::cout << maxCapacites[targetLength];
 
     return 0;
 }
