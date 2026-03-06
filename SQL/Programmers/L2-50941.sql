1390ec2113afc9505368ce0592767436e48b4fb8
footprint\Donghyeon <gusdldi300@naver.com>
2026-03-07 03:40:51 +0900

    solved

---
+++ SQL/Programmers/L2-50941.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59041
+
+SELECT NAME, COUNT(NAME) AS COUNT
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+GROUP BY NAME
+HAVING COUNT > 1
+ORDER BY NAME;
