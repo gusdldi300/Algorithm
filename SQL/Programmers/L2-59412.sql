1ce826d040a97f09f7551a1817ce2f34450eee76
footprint\Donghyeon <gusdldi300@naver.com>
2026-03-11 17:16:12 +0900

    solved

---
+++ SQL/Programmers/L2-59412.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59412
+
+SELECT HOUR(DATETIME) AS HOUR, COUNT(HOUR(DATETIME))
+FROM ANIMAL_OUTS
+GROUP BY HOUR
+HAVING HOUR >= 9 AND
+       HOUR <= 19
+ORDER BY HOUR;
