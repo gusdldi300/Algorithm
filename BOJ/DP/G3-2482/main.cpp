9f34af4f8c7afe1eb447306e78feb2f3b94cdac2
footprint\Donghyeon <gusdldi300@naver.com>
2026-04-27 18:44:21 +0900

    solved

--- BOJ/DP/G3-2482/main.cpp
+++ BOJ/DP/G3-2482/main.cpp
@@ -7,52 +7,51 @@
 
 int sCasesCounts[MAX_COLORS_COUNT][MAX_SELECT_COUNT];
 
-unsigned int sColorsCount;
-unsigned int sSelectedCount;
-
-static int GetSelectedCasesRecursive(unsigned int colorsIndex, unsigned int maxColorsCount, unsigned int selectedCount)
+static int GetSelectedCasesRecursive(int colorsCount, int selectCount)
 {
-    if (colorsIndex >= maxColorsCount)
+    if (colorsCount < ((2 * selectCount) - 1))
     {
         return 0;
     }
 
-    if (sCasesCounts[colorsIndex][selectedCount] >= 0)
+    if (selectCount == 1)
     {
-        return sCasesCounts[colorsIndex][selectedCount];
+        return colorsCount;
     }
 
-    unsigned int updatedCount = selectedCount;
-    
-    int casesCount = 0;
-    casesCount += GetSelectedCasesRecursive(colorsIndex + 1, maxColorsCount, updatedCount);
-    casesCount %= DIVISOR;
-
-    ++updatedCount;
-    if (updatedCount == sSelectedCount)
+    if (sCasesCounts[colorsCount][selectCount] != -1)
     {
-        ++casesCount;
-
-        updatedCount = 0;
+        return sCasesCounts[colorsCount][selectCount];
     }
 
-    casesCount += GetSelectedCasesRecursive(colorsIndex + 2, maxColorsCount, updatedCount);
+    int casesCount = GetSelectedCasesRecursive(colorsCount - 2, selectCount - 1);
+    casesCount += GetSelectedCasesRecursive(colorsCount - 1, selectCount);
     casesCount %= DIVISOR;
 
-    sCasesCounts[colorsIndex][selectedCount] = casesCount;
+    sCasesCounts[colorsCount][selectCount] = casesCount;
 
     return casesCount;
 }
 
 int main()
 {
-    std::cin >> sColorsCount >> sSelectedCount;
+    unsigned int colorsCount;
+    unsigned int selectedCount;
+
+    std::cin >> colorsCount >> selectedCount;
     
     memset(sCasesCounts, -1, sizeof(sCasesCounts));
-    int casesCount = GetSelectedCasesRecursive(2, sColorsCount - 1, 1);
+    for (int colorsIndex = 0; colorsIndex < colorsCount; ++colorsIndex)
+    {
+        sCasesCounts[colorsIndex][0] = 1;
+    }
+    
+    // Select first number
+    int casesCount = GetSelectedCasesRecursive(colorsCount - 3, selectedCount - 1);
+    
+    // Don't select first number
+    casesCount += GetSelectedCasesRecursive(colorsCount - 1, selectedCount);
 
-    memset(sCasesCounts, -1, sizeof(sCasesCounts));
-    casesCount += GetSelectedCasesRecursive(1, sColorsCount, 0);
     casesCount %= DIVISOR;
 
     std::cout << casesCount;
