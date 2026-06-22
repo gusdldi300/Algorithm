b5f78ce6619c2441b08829df833244eb4dd236df
footprint\Donghyeon <gusdldi300@naver.com>
2026-06-23 02:10:52 +0900

    reviewed

--- BOJ/DP/G4-20166/main.cpp
+++ BOJ/DP/G4-20166/main.cpp
@@ -1,10 +1,11 @@
 
+#include <cstring>
 #include <iostream>
 #include <cassert>
 
 #define MAX_MAP_SIZE (10U)
 #define MAX_STRING_SIZE (6U)
-#define MAX_DIRECTIONS_SIZE (8U)
+#define MAX_MOVES_SIZE (8U)
 
 struct Position
 {
@@ -17,7 +18,7 @@ static char sCharMap[MAX_MAP_SIZE][MAX_MAP_SIZE] = { 0, };
 static int sMapRowSize;
 static int sMapColSize;
 
-static Position MOVE_POSITIONS[] = {
+static Position MOVE_POSITIONS[MAX_MOVES_SIZE] = {
     { -1, 0 },
     { -1, 1 },
     { 0, 1 },
@@ -28,31 +29,18 @@ static Position MOVE_POSITIONS[] = {
     { -1, -1 },
 };
 
-static unsigned int GetStringCountRecursive(const std::string& targetString, Position position, unsigned int stringIndex, unsigned int caseCounts[MAX_STRING_SIZE][MAX_MAP_SIZE][MAX_MAP_SIZE])
+int GetTargetStringCasesCountRercursive(Position position, unsigned int stringIndex, const std::string& targetString, int targetStringCasesCount[MAX_STRING_SIZE][MAX_MAP_SIZE][MAX_MAP_SIZE])
 {
-    assert(stringIndex < targetString.size());
-
-    if (sCharMap[position.Row][position.Col] != targetString[stringIndex])
-    {
-        return 0;
-    }
-    
-    if (stringIndex == targetString.size() - 1)
+    if (stringIndex == targetString.size())
     {
         return 1;
     }
-
-    if (caseCounts[stringIndex][position.Row][position.Col] > 0)
+    
+    int totalCasesCount = 0;
+    for (unsigned int moveIndex = 0; moveIndex < MAX_MOVES_SIZE; ++moveIndex)
     {
-        return caseCounts[stringIndex][position.Row][position.Col];
-    }
-
-    unsigned int count = 0;
-
-    for (unsigned int dirIndex = 0; dirIndex < MAX_DIRECTIONS_SIZE; ++dirIndex)
-    {
-        int nextRow = position.Row + MOVE_POSITIONS[dirIndex].Row;
-        int nextCol = position.Col + MOVE_POSITIONS[dirIndex].Col;
+        int nextRow = position.Row + MOVE_POSITIONS[moveIndex].Row;
+        int nextCol = position.Col + MOVE_POSITIONS[moveIndex].Col;
 
         if (nextRow < 0)
         {
@@ -72,19 +60,33 @@ static unsigned int GetStringCountRecursive(const std::string& targetString, Pos
             nextCol = 0;
         }
 
-        count += GetStringCountRecursive(targetString, { nextRow, nextCol }, stringIndex + 1, caseCounts);
+        if (sCharMap[nextRow][nextCol] != targetString[stringIndex])
+        {
+            continue;
+        }
+
+        if (targetStringCasesCount[stringIndex + 1][nextRow][nextCol] != -1)
+        {
+            totalCasesCount += targetStringCasesCount[stringIndex + 1][nextRow][nextCol];
+
+            continue;
+        }
+
+        totalCasesCount += GetTargetStringCasesCountRercursive({ nextRow, nextCol }, stringIndex + 1, targetString, targetStringCasesCount);
     }
 
-    caseCounts[stringIndex][position.Row][position.Col] = count;
-
-    return count;
+    targetStringCasesCount[stringIndex][position.Row][position.Col] = totalCasesCount;
+    
+    return totalCasesCount;
 }
 
 int main()
 {
+    std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+
     unsigned int stringsCount;
     std::cin >> sMapRowSize >> sMapColSize >> stringsCount;
-    
     
     for (int row = 0; row < sMapRowSize; ++row)
     {
@@ -102,20 +104,24 @@ int main()
         std::string string;
         std::cin >> string;
 
-        unsigned int caseCounts[MAX_STRING_SIZE][MAX_MAP_SIZE][MAX_MAP_SIZE] = { 0, };
-        
-        unsigned int totalCount = 0;
+        int targetStringCasesCount[MAX_STRING_SIZE][MAX_MAP_SIZE][MAX_MAP_SIZE];
+        memset(targetStringCasesCount, -1, sizeof(targetStringCasesCount));
+
+        int totalCasesCount = 0;
         for (int row = 0; row < sMapRowSize; ++row)
         {
             for (int col = 0; col < sMapColSize; ++col)
             {
-                unsigned int count = GetStringCountRecursive(string, { row, col }, 0, caseCounts);
-                
-                totalCount += count;
+                if (sCharMap[row][col] != string[0])
+                {
+                    continue;
+                }
+
+                totalCasesCount += GetTargetStringCasesCountRercursive({ row, col }, 1, string, targetStringCasesCount);
             }
         }
 
-        std::cout << totalCount << '\n';
+        std::cout << totalCasesCount << '\n';
     }
 
     return 0;
