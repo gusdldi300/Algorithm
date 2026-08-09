51aaa3a33590fe810a72728113c5e60fe65c6335
footprint\Donghyeon <gusdldi300@naver.com>
2026-08-09 22:21:19 +0900

    reviewed

--- BOJ/DP/G5-1351/main.cpp
+++ BOJ/DP/G5-1351/main.cpp
@@ -2,24 +2,28 @@
 #include <iostream>
 #include <unordered_map>
 
-static std::unordered_map<unsigned long long, unsigned long long> InfiniteSequenceValues;
+static unsigned long long sFirstDivisor;
+static unsigned long long sSecondDivisor;
 
-static unsigned long long GetInfiniteSequenceRecursive(unsigned long long index, const unsigned long long firstDivisor, const unsigned long long secondDivisor)
+static std::unordered_map<unsigned long long, unsigned long long> sInfiniteSequenceValues;
+
+static unsigned long long GetInfiniteSequenceRecursive(unsigned long long index)
 {
     if (index == 0)
     {
         return 1ULL;
     }
 
-    if (InfiniteSequenceValues.find(index) != InfiniteSequenceValues.end())
+    if (sInfiniteSequenceValues.find(index) != sInfiniteSequenceValues.end())
     {
-        return InfiniteSequenceValues.find(index)->second;
+        return sInfiniteSequenceValues.find(index)->second;
     }
 
-    unsigned long long result = GetInfiniteSequenceRecursive(index / firstDivisor, firstDivisor, secondDivisor) +
-        GetInfiniteSequenceRecursive(index / secondDivisor, firstDivisor, secondDivisor);
+    unsigned long long result = 
+        GetInfiniteSequenceRecursive(index / sFirstDivisor) +
+        GetInfiniteSequenceRecursive(index / sSecondDivisor);
 
-    InfiniteSequenceValues.insert(std::pair<unsigned long long, unsigned long long>(index, result));
+    sInfiniteSequenceValues.insert(std::pair<unsigned long long, unsigned long long>(index, result));
 
     return result;
 }
@@ -27,12 +31,9 @@ static unsigned long long GetInfiniteSequenceRecursive(unsigned long long index,
 int main()
 {
     unsigned long long targetIndex;
-    unsigned long long firstDivisor;
-    unsigned long long secondDivisor;
+    std::cin >> targetIndex >> sFirstDivisor >> sSecondDivisor;
 
-    std::cin >> targetIndex >> firstDivisor >> secondDivisor;
-
-    std::cout << GetInfiniteSequenceRecursive(targetIndex, firstDivisor, secondDivisor);
+    std::cout << GetInfiniteSequenceRecursive(targetIndex);
 
     return 0;
 }
